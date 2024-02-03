f2c9029cc583b0f6a3d384b4f5b114d2ac5e6527
Ali Iqbal <aliiqbal436@gmail.com>
2024-02-03 22:45:58 +0500

    added code deployed yml files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# navigate to app folder
-cd /app
+set -e
 
-# install dependencies
-npm install
-npm run build
-cp -r build/* /var/www/html
-npm install pm2 -g
+# Deploy the React app to the target directory
+sudo cp -R * /var/www/html/react-app/
+
+# Start the Nginx service
+sudo service nginx start
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# navigate to app folder
-cd /app
+set -e
 
-# install node and npm
-yum -y install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-yum -y install nodejs
-yum -y install npm
-sudo yum -y install nginx
-
-# Enable and start the Nginx service
-sudo systemctl enable nginx
-sudo systemctl start nginx
-
-# Allow HTTP traffic in the firewall
+# Stop the Nginx service
+sudo service nginx stop
