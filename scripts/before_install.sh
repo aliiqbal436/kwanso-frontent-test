64a54c859a16402d2c471a87f4b1b9f9cef36d53
Ali Iqbal <aliiqbal436@gmail.com>
2024-02-03 22:27:27 +0500

    added code deployed yml files

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,5 +8,12 @@ yum -y install curl
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 yum -y install nodejs
 yum -y install npm
-yum -y install nginx
-ufw allow 'Nginx HTTP'
+sudo yum -y install nginx
+
+# Enable and start the Nginx service
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# Allow HTTP traffic in the firewall
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --reload
